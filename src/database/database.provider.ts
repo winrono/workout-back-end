a3d1d92958f6dc976155be658177c7d991d939f9
Roma Nosovich <r.nosovich@paralect.com>
2020-04-01 12:43:06 +0300

    more testing

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -5,31 +5,31 @@ export const databaseProvider =
 {
     provide: 'SEQUELIZE',
     //local config
-    // useFactory: async () => {
-    //   const sequelize = new Sequelize({
-    //     dialect: 'postgres',
-    //     host: 'localhost',
-    //     port: 5432,
-    //     username: 'postgres',
-    //     password: 'asd123!@',
-    //     database: 'workout_storage',
-    //   });
-    //   sequelize.addModels([User]);
-    //   await sequelize.sync();
-    //   return sequelize;
-    // },
-    //remote config
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
-        host: 'workoutstorage.cofhmrxxmfqy.eu-central-1.rds.amazonaws.com',
+        host: 'localhost',
         port: 5432,
         username: 'postgres',
-        password: process.env.SQL_PASSWORD,
+        password: 'asd123!@',
         database: 'workout_storage',
       });
       sequelize.addModels([User]);
       await sequelize.sync();
       return sequelize;
     },
+    //remote config
+    // useFactory: async () => {
+    //   const sequelize = new Sequelize({
+    //     dialect: 'postgres',
+    //     host: 'workoutstorage.cofhmrxxmfqy.eu-central-1.rds.amazonaws.com',
+    //     port: 5432,
+    //     username: 'postgres',
+    //     password: process.env.SQL_PASSWORD,
+    //     database: 'workout_storage',
+    //   });
+    //   sequelize.addModels([User]);
+    //   await sequelize.sync();
+    //   return sequelize;
+    // },
 };
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly _tasksService: TasksService){
+
+    }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this._tasksService.getAllTasks();
+    }
+}
